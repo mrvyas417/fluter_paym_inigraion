9f8ea8b4f7761c03db32c9dd17e08edb2ccca1c6
shubham.vyas <shubham.vyas@sunarctechnologies.com>
2022-03-09 13:41:54 +0530

    all chnage

--- app.js
+++ app.js
@@ -1,169 +1,67 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-
 const https = require('https');
+/*
+* import checksum generation utility
+* You can get this utility from https://developer.paytm.com/docs/checksum/
+*/
+const PaytmChecksum = require('./PaytmChecksum');
 
-const checksum_lib = require('./checksum');
+var paytmParams = {};
 
+paytmParams.body = {
+    "requestType"   : "Payment",
+    "mid"           : "gjwXDO79112149786380",
+    "websiteName"   : "WEBSTAGING",
+    "orderId"       : "ORDERID_98765",
+    "callbackUrl"   : "https://paytm-inti-backend-flutter.herokuapp.com/",
+    "txnAmount"     : {
+        "value"     : "100.00",
+        "currency"  : "INR",
+    },
+    "userInfo"      : {
+        "custId"    : "CUST_001",
+    },
+};
 
-app.use(express.json()); 
-app.use(express.urlencoded());
+/*
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), "uK3ozrlWSmaW_hti").then(function(checksum){
 
-app.get('/', function(req, res) {
-    console.log(req);
-    res.send(JSON.stringify({ Hello: 'World' }));
-});
-app.post('/generateTxnToken', function(request, res) {
-    console.log(request.body);
+    paytmParams.head = {
+        "signature"    : checksum
+    };
 
-    var paytmParams = {};
+    var post_data = JSON.stringify(paytmParams);
 
-    var MID = request.body.mid;
-    var orderId = request.body.orderId;
+    var options = {
 
-    var amount = parseFloat(String(request.body.amount));
-    var custId = request.body.custId;
-    var key_secret = request.body.key_secret;
-    var callbackUrl = request.body.callbackUrl;
-    var mode = request.body.mode;
-    var website = request.body.website;
-    var testing = String(request.body.testing);
-    console.log(callbackUrl);
-    console.log(mode);
+        /* for Staging */
+        hostname: 'securegw-stage.paytm.in',
 
+        /* for Production */
+        // hostname: 'securegw.paytm.in',
 
-    paytmParams.body = {
+        port: 443,
+        path: '/theia/api/v1/initiateTransaction?mid=gjwXDO79112149786380&orderId=ORDERID_98765',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
 
-        /* for custom checkout value is 'Payment' and for intelligent router is 'UNI_PAY' */
-        "requestType": "Payment",
+    var response = "";
+    var post_req = https.request(options, function(post_res) {
+        post_res.on('data', function (chunk) {
+            response += chunk;
+        });
 
-        /* Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
-        "mid": gjwXDO79112149786380,
+        post_res.on('end', function(){
+            console.log('Response: ', response);
+        });
+    });
 
-        /* Find your Website Name in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
-        "websiteName": website == 'WEBSTAGING' ? "DEFAULT" : website,
-
-        /* Enter your unique order id */
-        "orderId": "12334",
-
-        /* on completion of transaction, we will send you the response on this URL */
-        // "callbackUrl": "https://mrdishant.com",
-        "callbackUrl": "https://securegw.paytm.in/theia/paytmCallback?ORDER_ID=12334"
-        ,
-
-        /* Order Transaction Amount here */
-        "txnAmount": {
-
-            /* Transaction Amount Value */
-            "value": amount,
-
-            /* Transaction Amount Currency */
-            "currency": "INR",
-        },
-
-        /* Customer Infomation here */
-        "userInfo": {
-
-            /* unique id that belongs to your customer */
-            "custId": "122",
-        },
-
-    };
-
-    console.log("Mode");
-    console.log(mode);
-
-    if (mode == "1") {
-        console.log("Mode 1 So Net Banking");
-        paytmParams.body[
-            "enablePaymentMode"] = [{
-            "mode": "NET_BANKING",
-        }]
-    } else if (mode == "0") {
-        console.log("Mode 0 So BALANCE");
-        paytmParams.body[
-            "enablePaymentMode"] = [{
-            "mode": "BALANCE",
-        }]
-    } else if (mode == "2") {
-        console.log("Mode 2 So UPI");
-        paytmParams.body[
-            "enablePaymentMode"] = [{
-            "mode": "UPI",
-        }]
-    } else if (mode == "3") {
-        console.log("Mode 3 So CC");
-        paytmParams.body[
-            "enablePaymentMode"] = [{
-            "mode": "CREDIT_CARD"
-        }]
-    }
-
-    console.log(JSON.stringify(paytmParams));
-
-    /**
-     * Generate checksum by parameters we have in body
-     * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
-     */
-    checksum_lib.genchecksumbystring(JSON.stringify(paytmParams.body), key_secret, (err, checksum) => {
-
-        if (err) {
-            return;
-        }
-
-        /* head parameters */
-        paytmParams.head = {
-
-            /* put generated checksum value here */
-            "signature": checksum
-        };
-
-        /* prepare JSON string for request */
-        var post_data = JSON.stringify(paytmParams);
-
-        var options = {
-
-
-
-            /* for Staging */
-
-
-            /* for Production */
-            hostname: testing == "0" ? 'securegw-stage.paytm.in' : 'securegw.paytm.in',
-
-            port: 443,
-            path: '/theia/api/v1/initiateTransaction?mid=' + MID + '&orderId=' + orderId,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': post_data.length
-            }
-        };
-
-        // Set up the request
-        var response = "";
-        var post_req = https.request(options, (post_res) => {
-            post_res.on('data', (chunk) => {
-                response += chunk;
-            });
-
-            post_res.on('end', () => {
-                console.log(orderId);
-                console.log(MID);
-                console.log('Response: ', response);
-                response = JSON.parse(response);
-                res.send(response.body.txnToken);
-                return 0;
-            });
-        });
-
-        // post the data
-        post_req.write(post_data);
-        post_req.end();
-    });
-});
-
-app.listen(port, function() {
-    console.log(`Example app listening on port !`);
+    post_req.write(post_data);
+    post_req.end();
 });
